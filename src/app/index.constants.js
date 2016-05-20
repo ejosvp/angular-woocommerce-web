6b5f749671aa61b3b9026e210648a1335890f1a4
Ernesto Jose Vargas Paz <ejosvp@gmail.com>
2016-05-20 11:50:50 -0500

    changed api endpoints

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -3,8 +3,8 @@
 
     angular
         .module('fuse')
-        .constant('WORDPRESS_API_URL', 'https://www.futbolvivo.tv/deli-ecommerce/api/')
-        .constant('WOOCOMMERCE_API_URL', 'https://www.futbolvivo.tv/deli-ecommerce/wc-api/v3')
+        .constant('WORDPRESS_API_URL', 'http://52.39.248.128:8080/api/')
+        .constant('WOOCOMMERCE_API_URL', 'http://52.39.248.128:8080/wc-api/v3')
         .constant('WEBSOCKETS_URL', 'wss://52.39.248.128:1234')
 
         .constant('USER_ROLE', {
